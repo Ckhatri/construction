10fdcaed2c1868c3908bd7b41a77cd631864f02e
ckhatri <chirag.k.khatri@gmail.com>
2015-07-21 10:49:02 -0500

    Added masthead

--- wp-content/themes/html5blank/index.php
+++ wp-content/themes/html5blank/index.php
@@ -23,6 +23,26 @@
 		</div>
 	</div>
 </header>
+<section id="mastHead"> 
+	<div class="mast">
+		<div class="container">
+			<div class="row vertical-align">
+				<!-- Spacer -->
+				<div class = "col-md-1">
+				</div>
+				<div class = "col-md-8">
+					<h1><span>"NAME"</span><br/> CONSTRUCTION</h1>
+					<p>The Best Construction Company in the World</p>
+					<button class = "btn btn-lg">Our History</button>
+					<button class = "btn btn-lg">View Projects</button>
+				</div>
+				<!-- Spacer -->
+				<div class = "col-md-2">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
 <?php get_footer(); ?>
 
