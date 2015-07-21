63f4d2908b36356cbeb7a4e877148e9340130609
ckhatri <chirag.k.khatri@gmail.com>
2015-07-21 15:07:21 -0500

    Moved footer to the footer.phpfile

--- wp-content/themes/html5blank/footer.php
+++ wp-content/themes/html5blank/footer.php
@@ -1,3 +1,36 @@
+<footer>
+	<div class = "container">
+		<div class = "row text-center">
+			<div class = "col-md-4"></div>
+			<div class = "col-md-4">
+				<img src="http://placehold.it/150x75">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae commodo arcu. Morbi bibendum a tellus nec gravida. Pellentesque et libero non nisi vehicula egestas. </p>
+				<i class="fa fa-twitter fa-2x"></i>
+				<i class="fa fa-facebook fa-2x"></i>
+				<i class="fa fa-google fa-2x"></i>
+			</div>
+			<div class = "col-md-4"></div>
+		</div>
+		<div class = "row text-center">
+			<div class = "col-md-2"></div>
+			<div class = "col-md-4">
+				<h3>Get in Touch</h3>
+				<p><i class="fa fa-phone"></i> ###-###-####</p>
+				<p><i class="fa fa-envelope-o"></i> person@email.site</p>
+				<p><i class="fa fa-map-marker"></i> 1000 Not A Real Place, Texas, ######, USA</p>
+
+			</div>
+			<div class = "col-md-4">
+				<h3>Business Hours</h3>
+				<p>Monday to Friday: 9AM to 9PM</p>
+				<p>Saturday: 9AM to 6PM</p>
+				<p>Sunday: Closed</p>
+			</div>
+			<div class = "col-md-2"></div>
+		</div>
+	</div>
+</footer>
+
 		<script>
 		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
 		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
