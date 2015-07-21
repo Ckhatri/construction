73387bc1e6983d41fe06b2388f0e0522a9f89e78
ckhatri <chirag.k.khatri@gmail.com>
2015-07-21 14:00:47 -0500

    goofing around with sample pages

--- wp-content/themes/html5blank/header.php
+++ wp-content/themes/html5blank/header.php
@@ -13,3 +13,28 @@
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
 </head>
 <body>
+<header>
+	<div class = "container">
+		<div class = "row vertical-align">
+			<!-- Spacer -->
+			<div class = "col-md-2">
+			</div>
+			<div class = "col-md-4">
+				<img src="http://placehold.it/150x75">
+			</div>
+			<div class = "col-md-6">
+				<div class ="menu">
+					<ul class = "text-uppercase">
+						<li class = "active">Home</li>
+						<li>Company</li>
+						<li>Services</li>
+						<li>Projects</li>
+						<li>Blog</li>
+						<li>Contact</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+
--- wp-content/themes/html5blank/page-sample-page.php
+++ wp-content/themes/html5blank/page-sample-page.php
@@ -7,10 +7,8 @@
 				<div class = "col-md-1">
 				</div>
 				<div class = "col-md-8">
-					<h1><span>"NAME"</span><br/> CONSTRUCTION</h1>
+					<h1><span>ABOUT US</span><br/></h1>
 					<p>The Best Construction Company in the World</p>
-					<button class = "btn btn-lg">Our History</button>
-					<button class = "btn btn-lg">View Projects</button>
 				</div>
 				<!-- Spacer -->
 				<div class = "col-md-2">
@@ -35,40 +33,6 @@
 Fusce congue cursus volutpat. Praesent at accumsan urna. Nunc elit dolor, tempus eu feugiat nec, tempus mattis sapien. In faucibus vehicula mollis. Vivamus ultricies risus quis ipsum pulvinar gravida. Quisque id lobortis lectus. Duis non arcu nec mauris sagittis dapibus eget tempus leo.</p>
 				<button class = "btn btn-lg">Our History</button>
 			</div>
-		</div>
-	</div>
-</section>
-<section id = "process">
-	<div class = "container">
-		<div class = "row vertical-align">
-			<div class = "col-md-2 processBox text-center">
-				<h1><span>01 <i class="fa fa-long-arrow-right fa-1x"></i></span></h1>
-				<h1>PROCESS</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae commodo arcu. Morbi bibendum a tellus nec gravida. Pellentesque et libero non nisi vehicula egestas. </p>
-			</div>
-			<!-- Spacer -->
-			<div class = "col-md-1"></div>
-			<div class = "col-md-2 processBox text-center">
-				<h1><span>02 <i class="fa fa-long-arrow-right fa-1x"></i></span></h1>
-				<h1>PROCESS</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae commodo arcu. Morbi bibendum a tellus nec gravida. Pellentesque et libero non nisi vehicula egestas. </p>
-			</div>
-			<!-- Spacer -->
-			<div class = "col-md-1"></div>
-			<div class = "col-md-2 processBox text-center">
-				<h1><span>03 <i class="fa fa-long-arrow-right fa-1x"></i></span></h1>
-				<h1>PROCESS</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae commodo arcu. Morbi bibendum a tellus nec gravida. Pellentesque et libero non nisi vehicula egestas. </p>
-			</div>
-			<!-- Spacer -->
-			<div class = "col-md-1"></div>
-			<div class = "col-md-2 processBox text-center">
-				<h1><span>04 <i class="fa fa-long-arrow-right fa-1x"></i></span></h1>
-				<h1>PROCESS</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae commodo arcu. Morbi bibendum a tellus nec gravida. Pellentesque et libero non nisi vehicula egestas. </p>
-			</div>
-			<!-- Spacer -->
-			<div class = "col-md-1"></div>
 		</div>
 	</div>
 </section>
