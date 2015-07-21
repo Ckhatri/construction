712223f6395701d4e131a21c503dfa319960b666
ckhatri <chirag.k.khatri@gmail.com>
2015-07-21 13:47:50 -0500

    Finished offer session

--- wp-content/themes/html5blank/index.php
+++ wp-content/themes/html5blank/index.php
@@ -2,8 +2,8 @@
 <header>
 	<div class = "container">
 		<div class = "row vertical-align">
+			<!-- Spacer -->
 			<div class = "col-md-2">
-				Spacer
 			</div>
 			<div class = "col-md-4">
 				<img src="http://placehold.it/150x75">
@@ -52,6 +52,7 @@
 			</div>
 			<div class = "col-md-6 text-center">
 				<h1>WHO WE ARE</h1>
+				<hr/>
 				<h2>Welcome to "NAME" Construction</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae commodo arcu. Morbi bibendum a tellus nec gravida. Pellentesque et libero non nisi vehicula egestas. Pellentesque imperdiet ullamcorper eleifend. Ut quis diam nunc. Duis a rhoncus quam, vitae dictum leo. Donec ac iaculis nulla. Pellentesque hendrerit maximus nibh, in malesuada lectus ullamcorper et.
 
@@ -94,6 +95,53 @@ Fusce congue cursus volutpat. Praesent at accumsan urna. Nunc elit dolor, tempus
 			<div class = "col-md-1"></div>
 		</div>
 	</div>
+</section>
+
+<section id="offer">
+	<div class = "container">
+		<div class = "row top text-center text-uppercase">
+			<div class = "col-md-12">
+				<h1>What We Offer</h1>
+				<p>Always dedicated and Devoted</p>
+				<hr/>
+			</div>
+		</div>
+		<div class = "row text-center">
+			<div class = "col-md-4 infobox">
+				<i class="fa fa-briefcase fa-4x"></i>
+				<h1>BUILDING</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae commodo arcu. Morbi bibendum a tellus nec gravida. Pellentesque et libero non nisi vehicula egestas. </p>
+			</div>
+			<div class = "col-md-4 infobox">
+				<i class="fa fa-briefcase fa-4x"></i>
+				<h1>BUILDING</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae commodo arcu. Morbi bibendum a tellus nec gravida. Pellentesque et libero non nisi vehicula egestas. </p>
+			</div>
+			<div class = "col-md-4 infobox">
+				<i class="fa fa-briefcase fa-4x"></i>
+				<h1>BUILDING</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae commodo arcu. Morbi bibendum a tellus nec gravida. Pellentesque et libero non nisi vehicula egestas. </p>
+			</div>
+		</div>
+		<div class = "row text-center">
+			<div class = "col-md-4 infobox">
+				<i class="fa fa-briefcase fa-4x"></i>
+				<h1>BUILDING</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae commodo arcu. Morbi bibendum a tellus nec gravida. Pellentesque et libero non nisi vehicula egestas. </p>
+			</div>
+			<div class = "col-md-4 infobox">
+				<i class="fa fa-briefcase fa-4x"></i>
+				<h1>BUILDING</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae commodo arcu. Morbi bibendum a tellus nec gravida. Pellentesque et libero non nisi vehicula egestas. </p>
+			</div>
+			<div class = "col-md-4 infobox">
+				<i class="fa fa-briefcase fa-4x"></i>
+				<h1>BUILDING</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae commodo arcu. Morbi bibendum a tellus nec gravida. Pellentesque et libero non nisi vehicula egestas. </p>
+			</div>
+		</div>
+	</div>
+
 </section>
 
 <?php get_footer(); ?>
