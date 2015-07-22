be761030af8edf369d0d8a52311cb059b186c1ca
ckhatri <chirag.k.khatri@gmail.com>
2015-07-22 10:04:55 -0500

    Added a map to the contact page

--- wp-content/themes/html5blank/page-contact.php
+++ wp-content/themes/html5blank/page-contact.php
@@ -39,4 +39,12 @@
 		</div>
 	</div>
 </section>
+
+<section id = "map">
+		<div class = "row">
+			<div class = "col-xs-12 col-s-12 col-md-12 col-lg-12">
+				<a href="https://www.google.com/maps/dir//211+North+Ervay,+Dallas,+TX+75201/@32.7821534,-96.7976129,17z/data=!4m13!1m4!3m3!1s0x864e99220027e345:0x5821f5dfa4274d24!2s211+North+Ervay,+Dallas,+TX+75201!3b1!4m7!1m0!1m5!1m1!1s0x864e99220027e345:0x5821f5dfa4274d24!2m2!1d-96.7976129!2d32.7821534"><img src="<?php bloginfo('template_url'); ?>/img/map1.png"></a>
+			</div>
+		</div>
+</section>
 <?php get_footer(); ?>
